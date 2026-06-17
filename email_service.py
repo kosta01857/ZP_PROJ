8fe48a544111cf31ca45a17b18021dd97ea4cc5c
Anja Mihajlov <anja.mihajlov@gmail.com>
2026-06-17 23:49:26 +0200

    fixed ascii

--- email_service.py
+++ email_service.py
@@ -2,8 +2,8 @@ import base64
 
 class EmailService:
     def toRadix64(self, message: bytes)-> str:
-        return base64.b64encode(message).decode("utf-8")
+        return base64.b64encode(message).decode("ascii")
 
     def fromRadix64(self, message:str)-> bytes:
-        return base64.b64decode(message.encode("utf-8"))
+        return base64.b64decode(message.encode("ascii"))
 
