fc41b82c26ce45181744f6d87e6c595484087009
kosta01856 <kosta012001@gmail.com>
2026-06-17 22:06:06 +0200

    add enc svc ks dec and enc

--- encryption_service.py
+++ encryption_service.py
@@ -1,7 +1,11 @@
 import cryptography
 import rsa
+from rsa_service import RsaService
 
 class EncryptionService:
+    def __init__(self):
+        self.rsaSvc = RsaService()
+
     def encryptMessage(self, message, Ks):
         pass
 
@@ -10,8 +14,9 @@ class EncryptionService:
         pass
 
     
-    def encryptKs(self, Ks , PU):
-        pass
+    def encryptKs(self, Ks: int, pub: rsa.PublicKey) -> bytes:
+        return self.rsaSvc.encryptMessage(Ks.to_bytes(16,"big"),pub)
 
-    def decryptKs(self, message , PU):
-        pass
+    def decryptKs(self, encryptedKs: bytes , priv: rsa.PrivateKey) -> int:
+        decryptedKsBytes = self.rsaSvc.decryptMessage(encryptedKs, priv)
+        return int.from_bytes(decryptedKsBytes, "big")
