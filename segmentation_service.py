7e451daef42b8458b0110c2e1047dccb06620586
kosta01856 <kosta012001@gmail.com>
2026-06-17 23:57:44 +0200

    fix segSvc ascii

--- segmentation_service.py
+++ segmentation_service.py
@@ -5,13 +5,13 @@ class SegmentationService:
 
     def split(self, message: str, user: User):
         chunks = []
-        byte_length = len(message.encode('utf-8'))
+        byte_length = len(message.encode('ascii'))
 
         while byte_length > 50000:
             chunk = message[0:50000]
             chunks.append(chunk)
             message = message[50000:]
-            byte_length = len(message.encode('utf-8'))
+            byte_length = len(message.encode('ascii'))
         chunks.append(message)
 
         fileName = self.resolveFileName(user)
